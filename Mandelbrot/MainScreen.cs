096cee2dccd2b88cde5528d7470c9157b0e54242
aeduin <aeduin@nanquanu.org>
2018-11-29 14:50:08 +0100

    the project is now good enough to hand in

--- Mandelbrot/MainScreen.cs
+++ Mandelbrot/MainScreen.cs
@@ -9,30 +9,149 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Mandelbrot {
-    //Takes a floating point number from 0 to 1 and outputs a color
-    public delegate Color FloatToColorMapper(float f);
-
     public partial class MainScreen : Form {
-        const int MANDELBROT_OFFSET_Y = 200;
+        const int MANDELBROT_OFFSET_Y = 90;
 
         public MainScreen() {
             InitializeComponent();
 
-            this.WindowState = FormWindowState.Maximized;
+            //initialize controls
+            Label centerXLabel = new Label() {
+                Text = "Center X",
+            };
+            SetRectangle(centerXLabel, new Point(0, 0));
+            SetRectangle(this.centerX, new Point(1, 0));
 
+            Label centerYLabel = new Label() {
+                Text = "Center Y",
+            };
+            SetRectangle(centerYLabel, new Point(0, 1));
+            SetRectangle(this.centerY, new Point(1, 1));
+
+            Label maxIterCountLabel = new Label() {
+                Text = "Max Iterations",
+            };
+            SetRectangle(this.maxIterCount, new Point(3, 0));
+            SetRectangle(maxIterCountLabel, new Point(2, 0));
+
+            Label scaleLabel = new Label() {
+                Text = "Scale",
+            };
+            SetRectangle(this.scale, new Point(3, 1));
+            SetRectangle(scaleLabel, new Point(2, 1));
+
+            Button okButton = new Button() {
+                Text = "OK",
+            };
+            okButton.Click += this.OnOkButtonClick;
+            SetRectangle(okButton, new Point(4, 1));
+
+            Label threadCountLabel = new Label() {
+                Text = "Number of Threads",
+            };
+            SetRectangle(this.threadCount, new Point(5, 0));
+            SetRectangle(threadCountLabel, new Point(4, 0));
+
+            this.Controls.AddRange(new Control[]{
+                centerXLabel,
+                centerYLabel,
+                maxIterCountLabel,
+                scaleLabel,
+                threadCountLabel,
+                okButton,
+                this.centerX,
+                this.centerY,
+                this.scale,
+                this.maxIterCount,
+                this.threadCount,
+                this.mandelbrotImage
+            });
+            
+            //add events
             this.Resize += OnFormResize;
+            this.MouseWheel += OnMouseWheel;
+            this.mandelbrotImage.MouseClick += OnMandelbrotClick;
 
-            this.Controls.Add(this.mandelBrotImage);
+            this.WindowState = FormWindowState.Maximized;
+            this.UpdateMandelbrotImage();
         }
 
-        private MandelBrotImage mandelBrotImage = new MandelBrotImage();
+        /// <summary>
+        /// Updates the values of the center coordinate NumericUpDowns
+        /// </summary>
+        private void OnMandelbrotClick(object sender, MouseEventArgs e) {
+            this.centerX.Value = (decimal)this.mandelbrotImage.Center.X;
+            this.centerY.Value = (decimal)this.mandelbrotImage.Center.Y;
+        }
 
+        /// <summary>
+        /// Zooms in and redraws the mandelbrotImage
+        /// </summary>
+        private void OnMouseWheel(object sender, MouseEventArgs e) {
+            double scrollValue = Math.Max(0.01, 1.0 - e.Delta / 500.0);
+            this.mandelbrotImage.ZoomScale *= scrollValue;
+            this.scale.Value *= (decimal)scrollValue;
+
+            //redraw the mandelbrotImage
+            mandelbrotImage.Invalidate();
+        }
+
+        private void OnOkButtonClick(object sender, EventArgs e) {
+            UpdateMandelbrotImage();
+        }
+
+        /// <summary>
+        /// Transfers all the values from the input fields to the mandelbrotImage object
+        /// </summary>
+        private void UpdateMandelbrotImage() {
+            this.mandelbrotImage.ZoomScale = (double)this.scale.Value;
+            this.mandelbrotImage.Center = new PointD((double)this.centerX.Value, (double)this.centerY.Value);
+            this.mandelbrotImage.MaxMandelNumber = (int)maxIterCount.Value;
+            this.mandelbrotImage.ThreadCount = (int)this.threadCount.Value;
+
+            this.mandelbrotImage.Invalidate();
+        }
+
+        /// <summary>
+        /// Sets the location and size of a control
+        /// </summary>
+        /// <param name="frameLocation">the location in a virtual frame</param>
+        private static void SetRectangle(Control control, Point frameLocation) {
+            Size rowMargin = new Size(10, 10);
+            int rowHeight = 40;
+            int[] columns = new int[] { 0, 100, 300, 400, 600, 700, 900 };
+            
+            control.Location =
+                new Point(columns[frameLocation.X] + rowMargin.Width, rowHeight * frameLocation.Y + rowMargin.Height);
+            control.Size =
+                new Size(columns[frameLocation.X + 1] - columns[frameLocation.X] - rowMargin.Width * 2, rowHeight - rowMargin.Height / 2);
+        }
+
+        /// <summary>
+        /// Updates the size of the mandelbrotImage control
+        /// </summary>
         private void OnFormResize(object sender, EventArgs e) {
-            this.mandelBrotImage.Size = new Size(this.ClientSize.Width, this.ClientSize.Height/* - MANDELBROT_OFFSET_Y*/);
-            this.mandelBrotImage.Location = new Point(0,0/* MANDELBROT_OFFSET_Y*/);
+            this.mandelbrotImage.Size = new Size(this.ClientSize.Width, this.ClientSize.Height - MANDELBROT_OFFSET_Y);
 
             //make sure the entire mandelbrot is redrawn
-            this.mandelBrotImage.Invalidate();
+            this.mandelbrotImage.Invalidate();
         }
+
+        //initialize controls
+        private MandelbrotImage mandelbrotImage = new MandelbrotImage() {
+            Location = new Point(0, MANDELBROT_OFFSET_Y),
+        };
+
+        private NumericUpDown centerX = new NumericUpDown() {
+                DecimalPlaces = 10,
+                Minimum = -100,
+            },
+            centerY = new NumericUpDown() {
+                DecimalPlaces = 10,
+                Minimum = -100,
+            },
+            scale = new NumericUpDown() { Value = 2, DecimalPlaces = 20},
+            maxIterCount = new NumericUpDown() { Minimum = 0, Maximum = 20000, Value = 600 },
+            threadCount = new NumericUpDown() { Minimum = 1, Maximum = 100, Value = 8 };
     }
 }
--- Mandelbrot/MandelBrotImage.cs
+++ Mandelbrot/MandelBrotImage.cs
@@ -2,65 +2,140 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Diagnostics;
 
 namespace Mandelbrot {
-    public class MandelBrotImage : Control {
-        public MandelBrotImage() {
+    public delegate Color MandelColorMapper(int mandelNumber);
+    public delegate PointD PixelToCoordinate(Point pixelCoords);
+
+    public class MandelbrotImage : Control {
+        public PointD Center { get; set; } = new PointD(0.0, 0.0);
+        public double ZoomScale { get; set; } = 2.0;
+        public int ThreadCount { get; set; } = 8; //experimentation found that 8 is the optimal value for my own computer
+
+        public MandelbrotImage() {
             this.DoubleBuffered = true;
+            this.MouseClick += OnMandelMouseClick;
+        }
+
+        /// <summary>
+        /// Changes the center of the image to the mouse position
+        /// </summary>
+        private void OnMandelMouseClick(object sender, MouseEventArgs e) {
+            var newCenter = DefaultPixelToMandelCoordsMapper(this.Center, this.ClientSize, this.ZoomScale)(e.Location);
+            this.Center = new PointD(newCenter.X, -newCenter.Y);
+            this.Invalidate();
         }
 
         protected override void OnPaint(PaintEventArgs e) {
             base.OnPaint(e);
+            
+            int offsetY = this.ClientSize.Height / ThreadCount;
+            int restOffsetY = this.ClientSize.Height % ThreadCount;
+            int width = this.ClientSize.Width;
+            int height = this.ClientSize.Height;
 
-            e.Graphics.DrawImage(MandelBrot.Generate(this.ClientSize, new PointF(.41f, .21f), .05f, defaultColorMapper ), new Point(0, 0));
+            Console.WriteLine(restOffsetY);
 
-            Color defaultColorMapper(float x) {
-                return RGB((float)Math.Pow(x, .1) * .25f - 0.1f, (float)Math.Pow(x, .7), (float)Math.Pow(x, .5) * .62f);
+            Thread[] threads = new Thread[ThreadCount];
+            Bitmap[] images = new Bitmap[ThreadCount];
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            
+            //generate images
+            for (int i = 0; i < ThreadCount; i++) {
+                //assign i to a new variable because i will be modified by the loop (i.e. the i++ statement)
+                int threadId = i;
+
+                int clipHeight = offsetY + (threadId < restOffsetY ? 1 : 0);
+                int totalOffsetY = offsetY * threadId + Math.Min(threadId, restOffsetY);
+
+                threads[threadId] = new Thread(() => {
+                    images[threadId] = this.Generate(this.ClientSize, new Rectangle(0, totalOffsetY, width, clipHeight),
+                        this.Center, this.ZoomScale, DefaultColorMapper);
+                });
+                threads[threadId].Start();
             }
+            
+            //wait for threads to finish and draw the generated images
+            for (int i = 0; i < ThreadCount; i++) {
+                threads[i].Join();
+
+                int totalOffsetY = offsetY * i + Math.Min(i, restOffsetY);
+
+                e.Graphics.DrawImage(images[i], new Point(0, totalOffsetY));
+            }
+
+            sw.Stop();
+            Console.WriteLine($"time: {sw.ElapsedMilliseconds}");
         }
 
-        public static Color RGB(float r, float g, float b) {
-            return Color.FromArgb((int)(r * 255), (int)(g * 255), (int)(b * 255));
-        }
-    }
+        public int MaxMandelNumber { get; set; }
+        public static readonly Color backgroundColor = Color.FromArgb(0, 0, 0);
 
-    public static class MandelBrot {
-        public static int MaxMandelNumber { get; set; } = 500;
-        public static Color backgroundColor = Color.FromArgb(0, 0, 0);
-
-        public static Bitmap Generate(Size canvasSize, PointF center, float scale, FloatToColorMapper colorFromIterationCount) {
+        /// <summary>
+        /// Generates the mandelbrot image
+        /// </summary>
+        public Bitmap Generate(Size totalCanvasSize, Rectangle clip, PointD mandelCenter, double scale, MandelColorMapper colorFromIterationCount) {
             //canvas will be the return value
-            Bitmap canvas = new Bitmap(canvasSize.Width, canvasSize.Height);
+            Bitmap canvas = new Bitmap(clip.Width, clip.Height);
 
-            for (int pixelX = 0; pixelX < canvasSize.Width; pixelX++) {
-                for (int pixelY = 0; pixelY < canvasSize.Height; pixelY++) {
-                    double mappedX = center.X + ((double)canvasSize.Width / (double)canvasSize.Height / -2 + (double)pixelX / (double)canvasSize.Height) * scale;
-                    double mappedY = center.Y + (-0.5 + (double)pixelY / (double)canvasSize.Height) * scale;
+            var pixelMapper = DefaultPixelToMandelCoordsMapper(mandelCenter, totalCanvasSize, scale);
 
-                    int? mandelNumber = MandelNumber(mappedX, mappedY);
+            Point pixelCoordinate = new Point(0, 0);
+            for (pixelCoordinate.Y = clip.Top; pixelCoordinate.Y < clip.Bottom; pixelCoordinate.Y++) {
+                for (pixelCoordinate.X = clip.Left; pixelCoordinate.X < clip.Right; pixelCoordinate.X++) {
 
-                    canvas.SetPixel(pixelX, pixelY, mandelNumber != null ?
-                        colorFromIterationCount((float)mandelNumber / (float)MaxMandelNumber)
-                        : backgroundColor);
+                    PointD mandelCoordinates = pixelMapper(pixelCoordinate);
+                    int? mandelNumber = MandelNumber(mandelCoordinates);
+                    Color pixelColor = mandelNumber != null ?
+                        colorFromIterationCount((int)mandelNumber)
+                        : backgroundColor;
+
+                    canvas.SetPixel(pixelCoordinate.X - clip.X, pixelCoordinate.Y - clip.Y, pixelColor);
                 }
             }
 
             return canvas;
         }
 
-        public static int? MandelNumber(double x, double y) {
+        /// <summary>
+        /// returns a lambda that maps pixel coordinates relative to the top-left of the MandelbrotImage
+        /// to the coordinates in the mandelbrot
+        /// </summary>        
+        public PixelToCoordinate DefaultPixelToMandelCoordsMapper(PointD center, Size canvasSize, double scale) {
+            double canvasWidth = canvasSize.Width;
+            double canvasHeight = canvasSize.Height;
+            double offsetX = canvasWidth / canvasHeight / -2.0;
+            const double offsetY = -0.5;
+
+            return (pixelCoords) => new PointD(
+                center.X + (offsetX + pixelCoords.X / canvasHeight) * scale,
+                -(center.Y + (offsetY + pixelCoords.Y / canvasHeight) * scale)
+            );
+        }
+
+        /// <summary>
+        /// Generates the mandel number for a given coordinate in the picture. Returns null if mandelNumber > this.maxMandelNumber
+        /// </summary>
+        /// <returns></returns>
+        public int? MandelNumber(PointD coordinates) {            
             double a = 0.0, b = 0.0;
             int result = 0;
+            //destructuring the PointD appears to be faster than passing the entire struct to the mandelTransform method for some reason
+            double x = coordinates.X, y = coordinates.Y;
 
             const double MAX_MANDEL_DISTANCE = 2.0;
 
-            while (Pythagoras(a, b) < MAX_MANDEL_DISTANCE) {
+            while (Pythagoras(a, b) < MAX_MANDEL_DISTANCE * MAX_MANDEL_DISTANCE) {
                 (a, b) = MandelTransform(a, b, x, y);
 
-                if (++result > MaxMandelNumber) {
+                if (++result > this.MaxMandelNumber) {
                     return null;
                 }
             }
@@ -68,13 +143,24 @@ namespace Mandelbrot {
             return result;
         }
 
-        private static (double, double) MandelTransform(double a, double b, double x, double y) {
+        /// <summary>
+        /// Applies the core function of the mandelbrot (z^2 + c)
+        /// </summary>
+        /// <returns>new value for a and b</returns>
+        private static (double newA, double newB) MandelTransform(double a, double b, double x, double y) {
             return (a * a - b * b + x,
                     2.0 * a * b + y);
         }
 
         public static double Pythagoras(double a, double b) {
-            return Math.Sqrt(a * a + b * b);
+            return a * a + b * b;
+        }
+
+        /// <summary>
+        /// The method that maps mandel numbers to a color
+        /// </summary>
+        public static Color DefaultColorMapper(int mandelNumber) {
+            return Color.FromArgb(Math.Min(255, mandelNumber / 2), Math.Min(255, mandelNumber * 2 / 3), Math.Min(255, mandelNumber));
         }
     }
 }
